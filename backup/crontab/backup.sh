e10ed9d5412fe58aa02f56c2a1c4ca14ed81c346
wac <i.wac.tax@gmail.com>
2023-08-23 08:22:52 +0800

    ♨

--- backup/crontab/backup.sh
+++ backup/crontab/backup.sh
@@ -6,11 +6,12 @@ set -ex
 
 BKDIR=$BACKUP/crontab
 mkdir -p $BKDIR
-
-txt=$(hostname).txt
-git pull -f
-crontab -l >$BKDIR/$txt
 cd $BKDIR
+
+git pull -f || git reset --hard origin/main
+txt=$(hostname).txt
+
+crontab -l >$txt
 git add $txt
 git add -u
 git commit -m"$hostname crontab backup" || exit 0
