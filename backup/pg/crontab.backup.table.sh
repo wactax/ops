a5841982d4d1ee8818f9f5e2a3c7c6669295e552
wac <i.wac.tax@gmail.com>
2023-08-23 08:31:46 +0800

    ♨

--- backup/pg/crontab.backup.table.sh
+++ backup/pg/crontab.backup.table.sh
@@ -5,4 +5,4 @@ cd $DIR
 set -ex
 
 bunx cep -c src -o lib
-cron_add "$((RANDOM % 60)) $((RANDOM % 23)) *" $DIR "sh -c 'PG_BACKUP_TABLE=1 ./lib/main.js'"
+cron_add "$((RANDOM % 60)) $((RANDOM % 23)) *" $DIR PG_BACKUP_TABLE=1 ./lib/main.js
