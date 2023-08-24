ea3cfbd11d8d01b882b446d0bcf5c4129b7997d2
wac <i.wac.tax@gmail.com>
2023-08-24 18:57:35 +0800

    ♨

--- backup/btrfs/cron.sh
+++ backup/btrfs/cron.sh
@@ -4,4 +4,4 @@ DIR=$(dirname $(realpath "$0"))
 cd $DIR
 set -ex
 
-cron_add "$((RANDOM % 60)) $((RANDOM % 23)) *" $DIR ./snapshot.sh
+cron_add "$((RANDOM % 60)) $((RANDOM % 23)) *" $DIR ./backup.sh
