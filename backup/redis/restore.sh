d9bd1408fa81566daeebd8e2dca9a0cb96b0c782
wacdev <i@wac.tax>
2023-08-25 15:39:36 +0800

    ♨

--- backup/redis/restore.sh
+++ backup/redis/restore.sh
@@ -2,11 +2,14 @@
 
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
-set -ex
+set -x
 
 load() {
   rclone lsjson $RCLONE_BAK/$1 | jq -r ".[].Name" | tail -1
   # redis-cli -h 127.0.0.1 -p 9970 -a $REDIS_PASSWORD --pipe $1
 }
 
-load KV
+for name in $REDIS_LI; do
+  echo $name
+  load $name
+done
