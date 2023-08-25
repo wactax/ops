8aa6b7c91f4c8fc8d4d9b1551ea3e512ca38f93f
wac <i.wac.tax@gmail.com>
2023-08-26 06:48:23 +0800

    ♨

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,8 +2,11 @@
 
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
+bak() {
+  cd $DIR/$1
+  direnv allow
+  direnv exec . ./backup.sh
+}
 set -ex
-
-./pg/backup.sh &
-./redis/backup.sh &
-wait
+bak redis
+bak pg
