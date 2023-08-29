d55563a1e33d3272646ec12fdc685faad0ec4fad
wacdev <i@wac.tax>
2023-08-29 13:59:05 +0800

    ♨

--- clash/run.sh
+++ clash/run.sh
@@ -4,8 +4,22 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
+if ! command -v cargo &>/dev/null; then
+  if [ -f "$HOME/.cargo/env" ]; then
+    source "$HOME/.cargo/env"
+  fi
+fi
+
 if ! [ -x "$(command -v clash)" ]; then
-  go install github.com/Dreamacro/clash@latest
+  if ! command -v go &>/dev/null; then
+    command -v rtx &>/dev/null && eval $(rtx env)
+  fi
+  if ! command -v go &>/dev/null; then
+    rtx install go
+  fi
+  if ! [ -x "$(command -v clash)" ]; then
+    go install github.com/Dreamacro/clash@latest
+  fi
 fi
 
 clash -f ./clash.yml
