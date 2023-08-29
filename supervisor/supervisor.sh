00e904e6a8feabd6954ba74ed8e362abd30b2b41
wacdev <i@wac.tax>
2023-08-29 13:38:52 +0800

    ♨

---
+++ supervisor/supervisor.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+DIR=$(realpath $0) && DIR=${DIR%/*}
+cd $DIR
+set -ex
+
+if ! command -v supervisorctl &>/dev/null; then
+  apt-get install -y supervisor
+  rsync -av ./os/ /
+fi
