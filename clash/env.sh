24fdff27e94a6e837198d847cac8c79fc28cb91c
wacdev <i@wac.tax>
2023-08-29 15:56:16 +0800

    ♨

---
+++ clash/env.sh
@@ -0,0 +1,4 @@
+export NODE_TLS_REJECT_UNAUTHORIZED=0
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+export HTTPS_PORXY=http://127.0.0.1:7890 HTTP_PORXY=http://127.0.0.1:7890 ALL_PROXY=socks5://127.0.0.1:7890
+
