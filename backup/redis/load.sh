602e508e97bf6057fd908f2553e84bc43e264e56
wacdev <i@wac.tax>
2023-09-01 05:40:16 +0800

    ♨

--- backup/redis/load.sh
+++ backup/redis/load.sh
@@ -16,12 +16,13 @@ load() {
   fp=$tmp/$name
   rclone copy $file $tmp
   ip=127.0.0.1 # 只对开发机做恢复
-  host_port $(eval echo \${$1_HOST_PORT})
-  local password=$(eval echo \${$1_PASSWORD})
-  zstd -d $fp -c | redis-cli -h $ip -p $port -a $password --pipe
+  echo $fp
 }
 
 for name in $REDIS_LI; do
   echo $name
-  load $name redis
+  fp=$(load $name redis)
+  host_port $(eval echo \${${name}_HOST_PORT})
+  password=$(eval echo \${${name}_PASSWORD})
+  zstd -d $fp -c | redis-cli -h $ip -p $port -a $password --pipe
 done
