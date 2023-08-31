3b8f90f33bf0653bdecee0729318c4e961560082
wacdev <i@wac.tax>
2023-09-01 06:00:12 +0800

    ♨

--- backup/redis/load.sh
+++ backup/redis/load.sh
@@ -3,6 +3,8 @@
 DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 
+set -e
+
 case $(uname -s) in
 Linux*)
   echo "为避免误操作，不在 Linux 上运行"
@@ -11,26 +13,17 @@ Linux*)
 esac
 
 source host_port.sh
-
-set -ex
+source ../rclone_load.sh
 
 load() {
-  bucket=$RCLONE_BAK/$2.$1
-  name=$(rclone lsjson $bucket | jq -r ".[].Name" | sort | tail -1)
-  file=$bucket/$name
-  # echo $ip $port
-  tmp=/tmp/$1
-  mkdir -p $tmp
-  fp=$tmp/$name
-  rclone copy $file $tmp
-  echo $fp
-}
-
-for name in $REDIS_LI; do
-  echo $name
-  fp=$(load $name redis)
+  name=$1
+  fp=$2
   host_port $(eval echo \${${name}_HOST_PORT})
   ip=127.0.0.1 # 只对开发机做恢复
   password=$(eval echo \${${name}_PASSWORD})
-  zstd -d $fp -c | redis-cli -h $ip -p $port -a $password --pipe
+  pv $fp | zstd -d -c | redis-cli -h $ip -p $port -a $password --pipe
+}
+
+for name in $REDIS_LI; do
+  rclone_load $name redis load
 done
