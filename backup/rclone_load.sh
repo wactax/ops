413b5641f86ebc06f60bdd30edf1c0ee6566497c
wacdev <i@wac.tax>
2023-09-02 18:45:39 +0800

    ♨

--- backup/rclone_load.sh
+++ backup/rclone_load.sh
@@ -8,12 +8,25 @@ rclone_load() {
   local name=$1
   echo -e "\n${GREEN}$name${NC}\n"
   local bucket=$RCLONE_BAK/$2.$name
-  local bak=$(rclone lsjson $bucket | jq -r ".[].Name" | sort | tail -1)
-  local tmp=/tmp/$bucket
-  mkdir -p $tmp
+  local json=$(rclone lsjson $bucket | jq -r ".[-1]")
+  local bak=$(echo $json | jq -r ".Name")
+  local tmp=/tmp/$(echo $bucket | tr -d ':')
   local fp=$tmp/$bak
   echo "→ $fp"
-  $rget $fp https://$CDN/$2.$name/$bak
+
+  bucket_bak=$bucket/$bak
+
+  local url=https://$CDN/$2.$name/$bak
+  if [[ $(echo $json | jq -r '.IsDir') == "true" ]]; then
+    mkdir -p $fp
+    for name in $(rclone lsjson $bucket_bak | jq -r '.[].Path'); do
+      $rget $fp/$name $url/$name
+    done
+  else
+    mkdir -p $tmp
+    $rget $fp $url
+  fi
+
   $3 $fp $name # load
   rm -rf $(dirname $fp)
 }
