4c109ae7817157aa22d6f9e0338de524c11205cb
wacdev <i@wac.tax>
2023-09-02 17:52:17 +0800

    ♨

--- backup/rclone_load.sh
+++ backup/rclone_load.sh
@@ -2,17 +2,18 @@
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+rget='wget -P -N -c --random-wait --retry-connrefused --waitretry=5 --tries=20 -O'
+
 rclone_load() {
   local name=$1
   echo -e "\n${GREEN}$name${NC}\n"
   local bucket=$RCLONE_BAK/$2.$name
   local bak=$(rclone lsjson $bucket | jq -r ".[].Name" | sort | tail -1)
-  local file=$bucket/$bak
-  # echo $ip $port
   local tmp=/tmp/$bucket
   mkdir -p $tmp
   local fp=$tmp/$bak
-  rclone copy --stats-one-line --progress $file $tmp
+  echo "→ $fp"
+  $rget $fp https://$CDN/$2.$name/$bak
   $3 $fp $name # load
   rm -rf $(dirname $fp)
 }
